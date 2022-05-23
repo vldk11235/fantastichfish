2978ad8bf8e69e7769f77104c68db2c753d57769
Vlad <vlad.kremenchuk@gmail.com>
2022-05-23 19:44:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  interval: 7000, 
+ 
+  // mousewheel: {
+  //   invert: true,
+    
+  // },
+  
+ 
+
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: true,
+  // },
+  
+  
+
+  speed: 1000,
+  
+  effect: 'fade',
+
+  fadeEffect: {
+    crossFade: true
+  },
+//  нерабочие функции узнать у хмары
+  
+  // autoHeight: true, вообще все пропадает
+ 
+  // hashNavigation: {
+  //   watchState: true,
+  // },
+ 
+  // slideToCLickedSlide: true,
+});
+
+
+
+
+// And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+// function onMouseEnter(e) {
+//   this._autoplay('stop');
+// }
+// function onMouseLeave(e) {
+//   this._autoplay();
+// }
